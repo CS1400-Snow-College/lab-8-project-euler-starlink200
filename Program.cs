9a2df618f1154ebc5bc0cbcafc34ca88a1a37ced
Caleb Roskelley <caleb.roskelley@students.snow.edu>
2024-10-30 15:03:43 -0600

    added files to project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        multiples3_5();
+    }
+
+    static void multiples3_5()
+    {
+        int sumOfNumbers = 0;
+        for(int i = 1; i < 1001; i++)
+        {
+            if(i%3 == 0 || i%5 == 0)
+            {
+                sumOfNumbers += i;
+            }
+        }
+        Console.WriteLine("The sum of all numbers divisible by 3 or 5 between 1 and 1000 is " + sumOfNumbers);
+    }
+}
